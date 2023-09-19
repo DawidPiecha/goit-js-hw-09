d3e186502be737f1a08c773c0db584e655ef945b
Dawid <dawidpiecha88@gmail.com>
2023-09-19 20:46:59 +0200

    01-color-switcher task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,8 +5,18 @@ const stopButton = document.querySelector('button[data-stop]');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+let timerID = null;
 
 startButton.addEventListener('click', () => {
-  const randomColor = getRandomHexColor();
-  body.style.backgroundColor = randomColor;
+  timerID = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timerID);
+  startButton.disabled = false;
 });
