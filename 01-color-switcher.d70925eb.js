3ffd4de60542bd4323e62440106d85f1eb71e4a3
DawidPiecha <124462558+DawidPiecha@users.noreply.github.com>
2023-09-19 17:59:53 +0000

    Deploying to gh-pages from @ DawidPiecha/goit-js-hw-09@0cfda4feb33bf619d48c916ca2ca80c72be8e938 🚀

---
+++ 01-color-switcher.d70925eb.js
@@ -0,0 +1,2 @@
+const t=document.body,o=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");o.addEventListener("click",(()=>{const o=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=o}));
+//# sourceMappingURL=01-color-switcher.d70925eb.js.map
