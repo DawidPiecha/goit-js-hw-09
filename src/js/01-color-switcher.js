0cfda4feb33bf619d48c916ca2ca80c72be8e938
Dawid <dawidpiecha88@gmail.com>
2023-09-19 19:58:49 +0200

    changes css styles to make the page eye-friendly

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const body = document.body;
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+});
